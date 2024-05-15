b3aef99a3a95661968edd7c4e63dbb2665e8e02d
rowpacmal <rowpacmal@outlook.com>
2024-05-16 00:06:44 +0200

    test

--- script.js
+++ script.js
@@ -6,94 +6,94 @@ let playCount = 0;
 let maxStreak = 0;
 
 function playGame(playerMove) {
-  const computerMove = makeComputerMove();
-  let matchResult;
-  let gameResult;
+	const computerMove = makeComputerMove();
+	let matchResult;
+	let gameResult;
 
-  if (playerMove === 'rock') {
-    switch (computerMove) {
-      case 'rock':
-        matchResult = `You picked <span class="play-txt">${playerMove}</span> and your opponent picked <span class="play-txt">${computerMove}</span>`;
-        gameResult = 'Too bad it was a Tie...';
-        break;
-      case 'paper':
-        matchResult = `You picked <span class="play-txt">${playerMove}</span> and your opponent picked <span class="play-txt">${computerMove}</span>`;
-        gameResult = 'Unfortunately, you lost!';
-        playCount = 0;
-        break;
-      case 'scissors':
-        matchResult = `You picked <span class="play-txt">${playerMove}</span> and your opponent picked <span class="play-txt">${computerMove}</span>`;
-        gameResult = 'Congratulations, you won!';
-        playCount += 1;
-        break;
-      default:
-        console.error('ERROR');
-        alert('ERROR');
-    }
-  } else if (playerMove === 'paper') {
-    switch (computerMove) {
-      case 'rock':
-        matchResult = `You picked <span class="play-txt">${playerMove}</span> and your opponent picked <span class="play-txt">${computerMove}</span>`;
-        gameResult = 'Congratulations, you won!';
-        playCount += 1;
-        break;
-      case 'paper':
-        matchResult = `You picked <span class="play-txt">${playerMove}</span> and your opponent picked <span class="play-txt">${computerMove}</span>`;
-        gameResult = 'Too bad it was a Tie...';
-        break;
-      case 'scissors':
-        matchResult = `You picked <span class="play-txt">${playerMove}</span> and your opponent picked <span class="play-txt">${computerMove}</span>`;
-        gameResult = 'Unfortunately, you lost!';
-        playCount = 0;
-        break;
-      default:
-        console.error('ERROR');
-        alert('ERROR');
-    }
-  } else if (playerMove === 'scissors') {
-    switch (computerMove) {
-      case 'rock':
-        matchResult = `You picked <span class="play-txt">${playerMove}</span> and your opponent picked <span class="play-txt">${computerMove}</span>`;
-        gameResult = 'Unfortunately, you lost!';
-        playCount = 0;
-        break;
-      case 'paper':
-        matchResult = `You picked <span class="play-txt">${playerMove}</span> and your opponent picked <span class="play-txt">${computerMove}</span>`;
-        gameResult = 'Congratulations, you won!';
-        playCount += 1;
-        break;
-      case 'scissors':
-        matchResult = `You picked <span class="play-txt">${playerMove}</span> and your opponent picked <span class="play-txt">${computerMove}</span>`;
-        gameResult = 'Too bad it was a Tie...';
-        break;
-      default:
-        console.error('ERROR');
-        alert('ERROR');
-    }
-  }
+	if (playerMove === 'rock') {
+		switch (computerMove) {
+			case 'rock':
+				matchResult = `You picked <span class="play-txt">${playerMove}</span> and your opponent picked <span class="play-txt">${computerMove}</span>`;
+				gameResult = 'Too bad it was a Tie...';
+				break;
+			case 'paper':
+				matchResult = `You picked <span class="play-txt">${playerMove}</span> and your opponent picked <span class="play-txt">${computerMove}</span>`;
+				gameResult = 'Unfortunately, you lost!';
+				playCount = 0;
+				break;
+			case 'scissors':
+				matchResult = `You picked <span class="play-txt">${playerMove}</span> and your opponent picked <span class="play-txt">${computerMove}</span>`;
+				gameResult = 'Congratulations, you won!';
+				playCount += 1;
+				break;
+			default:
+				console.error('ERROR');
+				alert('ERROR');
+		}
+	} else if (playerMove === 'paper') {
+		switch (computerMove) {
+			case 'rock':
+				matchResult = `You picked <span class="play-txt">${playerMove}</span> and your opponent picked <span class="play-txt">${computerMove}</span>`;
+				gameResult = 'Congratulations, you won!';
+				playCount += 1;
+				break;
+			case 'paper':
+				matchResult = `You picked <span class="play-txt">${playerMove}</span> and your opponent picked <span class="play-txt">${computerMove}</span>`;
+				gameResult = 'Too bad it was a Tie...';
+				break;
+			case 'scissors':
+				matchResult = `You picked <span class="play-txt">${playerMove}</span> and your opponent picked <span class="play-txt">${computerMove}</span>`;
+				gameResult = 'Unfortunately, you lost!';
+				playCount = 0;
+				break;
+			default:
+				console.error('ERROR');
+				alert('ERROR');
+		}
+	} else if (playerMove === 'scissors') {
+		switch (computerMove) {
+			case 'rock':
+				matchResult = `You picked <span class="play-txt">${playerMove}</span> and your opponent picked <span class="play-txt">${computerMove}</span>`;
+				gameResult = 'Unfortunately, you lost!';
+				playCount = 0;
+				break;
+			case 'paper':
+				matchResult = `You picked <span class="play-txt">${playerMove}</span> and your opponent picked <span class="play-txt">${computerMove}</span>`;
+				gameResult = 'Congratulations, you won!';
+				playCount += 1;
+				break;
+			case 'scissors':
+				matchResult = `You picked <span class="play-txt">${playerMove}</span> and your opponent picked <span class="play-txt">${computerMove}</span>`;
+				gameResult = 'Too bad it was a Tie...';
+				break;
+			default:
+				console.error('ERROR');
+				alert('ERROR');
+		}
+	}
 
-  if (playCount > maxStreak) {
-    maxStreak = playCount;
-  }
+	if (playCount > maxStreak) {
+		maxStreak = playCount;
+	}
 
-  matchText.innerHTML = 'Loading...';
-  gameText.innerHTML = 'Loading...';
+	matchText.innerHTML = 'Loading...';
+	gameText.innerHTML = 'Loading...';
 
-  function printResult() {
-    matchText.innerHTML = matchResult;
-    gameText.innerHTML = gameResult;
-    winText.innerHTML = playCount;
-    winStreak.innerHTML = maxStreak;
-  }
+	function printResult() {
+		matchText.innerHTML = matchResult;
+		gameText.innerHTML = gameResult;
+		winText.innerHTML = playCount;
+		winStreak.innerHTML = maxStreak;
+	}
 
-  setTimeout(printResult, 100);
+	setTimeout(printResult, 100);
 }
 
 function makeComputerMove() {
-  const makeMove = Math.floor(Math.random() * 3);
-  let computerMove;
+	const makeMove = Math.floor(Math.random() * 3);
+	let computerMove;
 
-  /*
+	/*
   if (makeMove === 0) {
     computerMove = 'rock';
   } else if (makeMove === 1) {
@@ -103,20 +103,22 @@ function makeComputerMove() {
   }
   */
 
-  switch (makeMove) {
-    case 0:
-      computerMove = 'rock';
-      break;
-    case 1:
-      computerMove = 'paper';
-      break;
-    case 2:
-      computerMove = 'scissors';
-      break;
-    default:
-      console.error('ERROR');
-      alert('ERROR');
-  }
+	switch (makeMove) {
+		case 0:
+			computerMove = 'rock';
+			break;
+		case 1:
+			computerMove = 'paper';
+			break;
+		case 2:
+			computerMove = 'scissors';
+			break;
+		default:
+			console.error('ERROR');
+			alert('ERROR');
+	}
 
-  return computerMove;
+	return computerMove;
 }
+
+// end
